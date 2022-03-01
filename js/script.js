71ebd5cd91cd7b1f7fa8d418138e890aea4131b0
Sajeda Akhter <sajedasaju85@gmail.com>
2022-03-01 15:49:52 +0600

    error added

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ const searchPhones = () => {
 
 }
 const cards = (phone, phoneContainer) => {
+    console.log(phone)
     const div = document.createElement('div');
     div.classList.add('col');
     div.innerHTML = ` <div class="card h-100">
@@ -18,38 +19,44 @@ const cards = (phone, phoneContainer) => {
             <h5 class="card-title"> <span >Phone Name : </span><span class="text-color  fw-bold">${phone.phone_name}</span></h5>
             <p class="card-text"><span >Brand Name : </span><span class="text-color fw-bold">${phone.brand}</span></p>
             <div class="card-footer">
-            <button onclick="searchPhones()" class="button">Phone Details</button>
+            <button onclick="loadDetails('${phone.slug}')" class="button">Phone Details</button>
       </div>
             
         </div>
     </div>`
     phoneContainer.appendChild(div);
 
+
 }
 const displaySearchPhones = phones => {
     const phoneContainer = document.getElementById('displayPhones');
+    const errorCOntainer = document.getElementById('phoneNotFound');
+    phoneContainer.textContent = '';
+    errorCOntainer.textContent = '';
     let count = 0;
-    // if(phoneContainer)
-    phones.slice(0, 5).forEach(phone => {
-        count++;
-        /* const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = ` <div class="card h-100">
-        <img src="${phone.image}" class="card-img-top" alt="...">
-        <div class="card-body text-center">
-            <h5 class="card-title"> <span class="fs-2">Phone Name : </span><span class="text-color fs-2 fw-bold">${phone.phone_name}</span></h5>
-            <p class="card-text"><span class="fs-2">Brand Name : </span><span class="text-color fs-2 fw-bold">${phone.brand}</span></p>
-            <button onclick="searchPhones()" class="button fs-4">Phone Details</button>
-        </div>
-    </div>`
-    phoneContainer.appendChild(div); */
-        cards(phone, phoneContainer);
-        // document.getElementById('show-all').style.display = 'block';
+    // console.log(phones.length)
 
+    if (phones.length <= 0) {
 
-    });
+        const div = document.createElement('div');
+        div.classList.add('d-flex', "justify-content-center", "align-items-center")
+        div.innerHTML = `<img class="img-fluid me-2" src="./images/cross.png" alt="" width="40px">
+        <h3>Phone is currently unavailable</h3>
+        `
+        errorCOntainer.appendChild(div);
+
+    }
+    else {
+
+        phones.slice(0, 5).forEach(phone => {
+            count++;
+            cards(phone, phoneContainer);
+        });
+    }
 
 }
+
+
 
 
 
