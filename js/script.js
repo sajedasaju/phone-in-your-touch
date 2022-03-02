45b6d876804c98d65f55c08e4a8e2382ec5ee91e
Sajeda Akhter <sajedasaju85@gmail.com>
2022-03-02 17:15:45 +0600

    responsive

--- js/script.js
+++ js/script.js
@@ -160,39 +160,3 @@ const closeCard = () => {
 
 }
 
-/* const showAllSearchPhones = (phones) => {
-    const searchFeild = document.getElementById('search-input');
-    const searchText = searchFeild.value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayAllSearchPhones(data.data))
-
-
-
-}
-const displayAllSearchPhones = (phones) => {
-    const phoneContainer = document.getElementById('displayPhones');
-    let count = 0;
-    console.log(phones)
-    // if(phoneContainer)
-    phones.slice(5).forEach(phone => {
-        count++;
-        //     const div = document.createElement('div');
-        //     div.classList.add('col');
-        //     div.innerHTML = ` <div class="card h-100">
-        //     <img src="${phone.image}" class="card-img-top" alt="...">
-        //     <div class="card-body text-center">
-        //         <h5 class="card-title"> <span class="fs-2">Phone Name : </span><span class="text-color fs-2 fw-bold">${phone.phone_name}</span></h5>
-        //         <p class="card-text"><span class="fs-2">Brand Name : </span><span class="text-color fs-2 fw-bold">${phone.brand}</span></p>
-        //         <button onclick="searchPhones()" class="button fs-4">Phone Details</button>
-        //     </div>
-        // </div>`
-        // phoneContainer.appendChild(div);
-        cards(phone, phoneContainer);
-        document.getElementById('show-all').style.display = 'block';
-
-        console.log(phone, count);
-
-    });
-} */
